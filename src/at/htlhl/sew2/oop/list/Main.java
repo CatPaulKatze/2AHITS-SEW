1be8df6a75c48e60dc4bbd4e0eba04804b55390d
Paul Sommer <paul@sommers.dev>
2025-12-16 13:25:50 +0100

    refactor: ShoppingBasket

--- src/at/htlhl/sew2/oop/list/Main.java
+++ src/at/htlhl/sew2/oop/list/Main.java
@@ -1,17 +1,16 @@
 package at.htlhl.sew2.oop.list;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class Main {
     static void main() {
-        List<String> einkaufsListe = new ArrayList<>();
+        ArrayList<String> einkaufsListe = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while(true) {
-            System. out.println("Brauchen wir noch etwas j/n");
+            System.out.println("Brauchen wir noch etwas j/n");
             String s = scanner.next();
-            if (s. equals("j")) {
+            if (s.equals("j")) {
                 System.out.println("Was brauchen wir noch?");
                 String produkt = scanner.next();
                 einkaufsListe.add(produkt);
---
+++ src/at/htlhl/sew2/oop/list/ShoppingBasket.java
@@ -0,0 +1,22 @@
+package at.htlhl.sew2.oop.list;
+
+import java.util.ArrayList;
+
+class ShoppingBasket {
+    void main() {
+        ArrayList<String> einkaufsListe = new ArrayList<>();
+        while (true) {
+            String s = IO.readln("Brauchen wir noch etwas j/n");
+            if (s.equals("j")) {
+                String produkt = IO.readln("Was brauchen wir noch?");
+                einkaufsListe.add(produkt);
+            } else if (s.equals("n")) {
+                IO.println("Hier ist die Einkaufsliste: ");
+                for (String produkt : einkaufsListe) {
+                    IO.println(produkt);
+                }
+                break;
+            }
+        }
+    }
+}
